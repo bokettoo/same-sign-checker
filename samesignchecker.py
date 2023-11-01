790910d267b4c6491213377ca372b36dfea77e77
Anass <146187264+bokettoo@users.noreply.github.com>
2023-11-01 12:45:40 +0100

    Add files via upload

---
+++ samesignchecker.py
@@ -0,0 +1,7 @@
+a = int(input("enter a number "))
+b = int(input("enter a number "))
+
+if a*b>0: 
+    print(f"{a} and {b} have the same sign")
+else: 
+    print(f"{a} and {b} don't have the same sign")
